b5b5083f461e3990468b40e943ddea94c02718b7
apadana1984 <im.khademi@gmail.com>
2019-09-11 19:02:49 -0700

    First working example

---
+++ TCA6424A.h
@@ -0,0 +1,21 @@
+/* TCA6424 REGISTERS and Addresses */
+// Command Byte
+
+#define INPUT_P0  0x00
+#define INPUT_P1  0x01
+#define INPUT_P2  0x02
+
+#define OUTPUT_P0  0x04
+#define OUTPUT_P1  0x05
+#define OUTPUT_P2  0x06
+
+#define POLINV_P0  0x08
+#define POLINV_P1  0x09
+#define POLINV_P2  0x0A
+
+#define CONFIG_P0  0x0C
+#define CONFIG_P1  0x0D
+#define CONFIG_P2  0x0E
+
+#define AUTOINCR_ON 0x80
+#define AUTOINCR_OFF 0x00
---
+++ TCA6424_ReadWrite.ino
@@ -0,0 +1,62 @@
+/******** TCA6424 on Texas Instrument's IO Expander Board *********/
+/*
+ * Author: Iman Khademi 2019
+ * MIT License
+ * 
+ * This is a demo for TCA6424 I2C port expander. The IC is on a Texas Instrument IO Expander EVM.
+ * This sketch does not use the INT signal. Check the repository for other sketches with the interrupt signal.
+ * Instead of reading the interrupt signal, it reads the input ports P00 and P01 (Two push buttons) every millisecond and maps them directly to the 
+ * LED ports (P02 and P03)
+ */
+
+#include <Wire.h>
+#include "TCA6424A.h"
+
+#define TCA6424_I2C_ADDR  0x22 // default I2C address
+
+void setup() {
+
+  Wire.begin();// Connect my Arduino to the I2C bus as master
+  
+  /* setup input-outputs here */
+  // WRITE SEQUENCE (See page 20 of the datasheet for TCA2464)
+  // 1: Send the Slave Address (I2C Address of the IO Expansion Board)
+  Wire.beginTransmission(TCA6424_I2C_ADDR);
+  // 2: Send the Command Byte
+  Wire.write(CONFIG_P0); // choosing P0 configuration register to write to
+  // 3: Send the Data
+  Wire.write(0xF3); // Set Pin 2 and 3 as output ports (set to zero), and Pins 1-2 and 4-7 as input ports (set to one)
+  // Execute the Sequence
+  Wire.endTransmission();
+   /******** FINISHED WRITE SEQUENCE **********/
+}
+
+void loop() {
+
+  // We read the push buttons to control the LEDs on TI IO Expander EVM
+
+  // READ SEQUENCE (See page 21 of the datasheet for TCA2464)
+  // 1: Send the Slave Address (I2C Address of the IO Expansion Board)
+  Wire.beginTransmission(TCA6424_I2C_ADDR);
+  // 2: Send the Command Byte
+  Wire.write(INPUT_P0); // choosing Input P0 register to read from
+  // 3: Send the Slave Address (I2C Address of the IO Expansion Board)
+  Wire.requestFrom(TCA6424_I2C_ADDR,1);
+  // 4: Read the Register
+  unsigned int c = Wire.read(); // Set Pin and 3 as output ports (set to zero), and Pins 1-2 and 4-7 as input ports (set to one)
+  // Execute the Sequence
+  Wire.endTransmission();
+  /* Display on the serial port */
+
+  // CONTROL THE LEDs via I2C
+  // WRITE SEQUENCE (See page 20 of the datasheet for TCA2464)
+  // 1: Send the Slave Address (I2C Address of the IO Expansion Board)
+  Wire.beginTransmission(TCA6424_I2C_ADDR);
+  // 2: Send the Command Byte
+  Wire.write(OUTPUT_P0); // choosing P0 configuration register to write to
+  // 3: Send the Data
+  Wire.write((byte)((c&0x03)<<2)); // Change the LED status based on the push buttons value
+  // Execute the Sequence
+  Wire.endTransmission();
+  delay(1);
+}
